7032bd032fa91f5b106d794924705555578e4f6b
Gleidson Medrado <gleidsonmedrado@hotmail.com>
2023-11-18 11:01:56 -0300

    Organizando projeto

---
+++ src/data/ProdutosExemplos.js
@@ -0,0 +1,124 @@
+const ProdutosExemplo = [
+    {
+        codigo:"1234",
+        marca: "Marca",
+        modelo: "Modelo1",
+        preco: 130,
+        descricao: "Belo Produto",
+        imagens: [
+            "/assets/01.jpg",
+            "/assets/02.jpg",
+            "/assets/03.jpg",
+
+         ],
+         promocao: false,
+    },
+
+    {
+        codigo:"1235",
+        marca: "Marca",
+        modelo: "Modelo1",
+        preco: 50,
+        descricao: "Belo Produto",
+        imagens: [
+            "/assets/04.jpg",
+            "/assets/05.jpg",
+            "/assets/06.jpg",
+
+         ],
+         promocao: true,
+    },
+
+    {
+        codigo:"1236",
+        marca: "Marca",
+        modelo: "Modelo1",
+        preco: 69,
+        descricao: "Belo Produto",
+        imagens: [
+            "/assets/07.jpg",
+            "/assets/08.jpg",
+            "/assets/09.jpg",
+
+         ],
+         promocao: false,
+    },
+
+    {
+        codigo:"1237",
+        marca: "Marca",
+        modelo: "Modelo1",
+        preco: 160,
+        descricao: "Belo Produto",
+        imagens: [
+            "/assets/10.jpg",
+            "/assets/11.jpg",
+            "/assets/12.jpg",
+
+         ],
+         promocao: false,
+    },
+
+    {
+        codigo:"1238",
+        marca: "Marca",
+        modelo: "Modelo1",
+        preco: 50,
+        descricao: "Belo Produto",
+        imagens: [
+            "/assets/13.jpg",
+            "/assets/14.jpg",
+            "/assets/15.jpg",
+
+         ],
+         promocao: true,
+    },
+
+    {
+        codigo:"1239",
+        marca: "Marca",
+        modelo: "Modelo1",
+        preco: 100,
+        descricao: "Belo Produto",
+        imagens: [
+            "/assets/16.jpg",
+            "/assets/17.jpg",
+            "/assets/18.jpg",
+
+         ],
+         promocao: false,
+    },
+
+    {
+        codigo:"1240",
+        marca: "Marca",
+        modelo: "Modelo1",
+        preco: 90,
+        descricao: "Belo Produto",
+        imagens: [
+            "/assets/19.jpg",
+            "/assets/20.jpg",
+            "/assets/21.jpg",
+
+         ],
+         promocao: false,
+    },
+
+    {
+        codigo:"1241",
+        marca: "Marca",
+        modelo: "Modelo1",
+        preco: 80,
+        descricao: "Belo Produto",
+        imagens: [
+            "/assets/22.jpg",
+            "/assets/23.jpg",
+            "/assets/24.jpg",
+
+         ],
+         promocao: true,
+    },
+
+
+]
+export default ProdutosExemplo;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react"
+import { createRoot } from "react-dom/client"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <h1>React App</h1>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Rotas from "./Rotas"
+
+const contentor = document.getElementById("root")
+const origem = createRoot(contentor)
+
+origem.render( <Rotas/> )
