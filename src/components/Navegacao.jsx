39ea8bb669b223696c917c3fbc8b5a85e84d353b
Leonardo Reis Gama <leonardorgama131@gmail.com>
2023-11-25 11:21:21 -0300

    Pronto

--- src/components/Navegacao.jsx
+++ src/components/Navegacao.jsx
@@ -9,36 +9,24 @@ const Modelo = styled.div`
     gap: 32px; 
     height: 32px; 
     padding: 32px; 
-
 `
-
 const ModeloTitulo = styled.div`
     font-size: 32pt;
     padding: 32px 0;
     text-align: center;
-`
-// const ModeloImagem = styled.img` 
-//   height: 48px; 
-// ` 
-
+` 
 const ModeloBotoes = styled.div`
     display: flex;
     gap: 32px;
     justify-content: center;
 `
-
 const ModeloLink = styled.a` 
   color: #fff; 
   text-decoration: none; 
 `
-
 export default function Navegacao(props) {
     return <Modelo>
-        {/* <ModeloImagem src="./logo.png" alt="logo"/>  */}
         <ModeloTitulo> { props.titulo } </ModeloTitulo>
         <ModeloBotoes> { props.children } </ModeloBotoes>
-        <ModeloLink href="#"> Filmes </ModeloLink> 
-        <ModeloLink href="#"> Séries </ModeloLink> 
-        <ModeloLink href="#"> Em alta </ModeloLink> 
     </Modelo>
 }
